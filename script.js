97091068216b2b24217b4ccc5845659eef727510
CRosal04 <156229740+CRosal04@users.noreply.github.com>
2024-10-01 14:22:50 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 document.getElementById("firstPara").innerHTML += "<span class ='tan'>This is new tan text.</span>"
 
 //selecting by tag name
-let images = document. getElementsByTagName ("img");
+let images = document.getElementsByTagName ("img");
 for( let image of images){
 image.width = "250";
 }
